de667c47d5abed8dd7cfa5d453de571abcb59d37
Aleksander Grzybowski <kelog@orange.pl>
2017-01-29 08:18:49 +0100

    Selecting contacts on the frontend

--- frontend/src/ContactManager.js
+++ frontend/src/ContactManager.js
@@ -3,13 +3,39 @@ import { Col, Table, Badge } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default class ContactManager extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedContactIds: [1]
+        }
+    }
+
+    toggleSelected = (id) => {
+        if (this.state.selectedContactIds.includes(id)) {
+            this.setState({
+                selectedContactIds: this.state.selectedContactIds.filter(i => i != id)
+            });
+        } else {
+            this.setState({
+                selectedContactIds: this.state.selectedContactIds.concat([id])
+            });
+        }
+    };
+
     render() {
         const groupCell = (groupName) => groupName.split(',').map(group => (
             <Badge key={group}>{group}</Badge>
         ));
-        
+
+        const isContactSelected = (id) => this.state.selectedContactIds.includes(id);
+
         const rows = this.props.contacts.map(contact => (
-            <tr key={contact.id}>
+            <tr
+                key={contact.id}
+                className={isContactSelected(contact.id) && 'success'}
+                onClick={() => this.toggleSelected(contact.id)}
+            >
                 <td>{contact.firstName}</td>
                 <td>{contact.lastName}</td>
                 <td>{groupCell(contact.groupName)}</td>
