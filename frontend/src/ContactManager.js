29a3c0c2392648c4be537fbfe4e4f59486e315f1
Aleksander Grzybowski <kelog@orange.pl>
2017-01-31 20:54:06 +0100

    Group select

--- frontend/src/ContactManager.js
+++ frontend/src/ContactManager.js
@@ -8,8 +8,20 @@ export default class ContactManager extends Component {
         super(props);
 
         this.state = {
-            selectedContactIds: []
-        }
+            selectedContactIds: [],
+            selectedGroups: []
+        };
+    }
+
+    getAllGroups() {
+        const allGroupsRepeating = this.props.contacts.map(contact => contact.groupName.split(','))
+            .reduce((left, right) => [...left, ...right], []);
+
+        return [...(new Set(allGroupsRepeating))].sort();
+    }
+
+    isGroupSelected(group) {
+        return this.state.selectedGroups.includes(group);
     }
 
     toggleSelected = (id) => {
@@ -24,15 +36,42 @@ export default class ContactManager extends Component {
         }
     };
 
+    toggleGroup = (group) => {
+        let selectedGroups = this.state.selectedGroups;
+        let selectedContactIds = this.state.selectedContactIds;
+
+        const filterByGroup = () => this.props.contacts.filter(contact => contact.groupName.includes(group));
+
+        if (this.isGroupSelected(group)) {
+            selectedGroups = selectedGroups.filter(g => g !== group);
+
+            filterByGroup().forEach(({id}) => {
+                if (selectedContactIds.includes(id)) {
+                    selectedContactIds = selectedContactIds.filter(cid => cid !== id)
+                }
+            });
+        } else {
+            selectedGroups.push(group);
+
+            filterByGroup().forEach(({id}) => {
+                if (!selectedContactIds.includes(id)) {
+                    selectedContactIds.push(id);
+                }
+            });
+        }
+
+        this.setState({selectedGroups, selectedContactIds});
+    };
+
     send = (content) => {
         const messages = this.state.selectedContactIds.map(id => ({contactId: id, content}));
         this.props.onSend(messages);
-        this.setState({selectedContactIds: []});
+        this.setState({selectedContactIds: [], selectedGroups: []});
     };
 
     render() {
         const groupCell = (groupName) => groupName.split(',').map(group => (
-            <Badge key={group}>{group}</Badge>
+            <Badge key={group} style={{marginRight: 5}}>{group}</Badge>
         ));
 
         const isContactSelected = (id) => this.state.selectedContactIds.includes(id);
@@ -50,6 +89,17 @@ export default class ContactManager extends Component {
             </tr>
         ));
 
+        const groupSelect = this.getAllGroups().map(group => (
+            <Badge
+                bsClass={'badge ' + (this.isGroupSelected(group) && 'alert-success')}
+                key={group}
+                onClick={() => this.toggleGroup(group)}
+                style={{cursor: 'pointer', marginRight: 5}}
+            >
+                {group}
+            </Badge>
+        ));
+
         return (
             <Col md={8} mdOffset={2}>
                 <Table style={{userSelect: 'none', cursor: 'default'}}>
@@ -65,6 +115,10 @@ export default class ContactManager extends Component {
                     {rows}
                     </tbody>
                 </Table>
+                <div style={{marginBottom: 10}}>
+                    <span style={{marginRight: 10}}>Zaznacz grupowo:</span>
+                    {groupSelect}
+                </div>
                 <NewMessageInput
                     sendingDisabled={this.state.selectedContactIds.length === 0}
                     onSend={this.send}
