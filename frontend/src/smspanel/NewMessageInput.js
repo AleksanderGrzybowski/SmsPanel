ccfd33199086e683787266a98e5f9942a075e516
Aleksander Grzybowski <kelog@orange.pl>
2017-02-10 11:04:47 +0100

    Fix pasting too long text, allow to edit before sending

--- frontend/src/smspanel/NewMessageInput.js
+++ frontend/src/smspanel/NewMessageInput.js
@@ -11,25 +11,26 @@ export default class NewMessageInput extends Component {
     }
 
     handleChange = (e) => {
-        if (e.target.value.length > 160) {
-            return;
-        }
         this.setState({text: e.target.value})
     };
 
 
     render() {
-        const style = (this.state.text.length > 140) ? {color: 'red'} : null;
-        const charsLeftMessage = (
-            <span style={style}>(pozostało: {160 - this.state.text.length})</span>
-        );
+        const charsLeftMessage = (this.state.text.length <= 160) ? (
+                <span>(pozostało: {160 - this.state.text.length})</span>
+            ) : (
+                <span style={{color: 'red'}}>(przekroczono: {this.state.text.length - 160})</span>
+            );
 
         const buttonContent = this.props.sendingInProgress ? (
                 <i className="fa fa-spin fa-spinner"/>
             ) : ( 'Wyślij!' );
 
-        const formDisabled = this.state.text.length === 0 || this.props.sendingInProgress || this.props.sendingDisabled;
-        
+        const formDisabled = this.state.text.length === 0
+            || this.state.text.length > 160
+            || this.props.sendingInProgress
+            || this.props.sendingDisabled;
+
         return (
             <form>
                 <FormGroup>
