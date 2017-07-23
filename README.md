07cae8ac58914f2d808a2756706e20579384a14f
Aleksander Grzybowski <kelog@orange.pl>
2017-07-23 18:22:10 +0200

    Polish text on error page

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This is a web application I created for my local church band to help us communicate. It is a nice web wrapper around bramka.gsmservice.pl bulk SMS sending service.
 
+The app does not use any i18n library - all texts are in Polish. I would definitely use one in any serious project, though :)
+
 Stack:
 * Grails, Gradle, Docker, external API
 * React+Redux, Bootstrap, Axios
--- frontend/src/smspanel/ErrorPage.js
+++ frontend/src/smspanel/ErrorPage.js
@@ -3,8 +3,8 @@ import ErrorAlert from './ErrorAlert';
 
 const ErrorPage = () => (
     <ErrorAlert>
-        <h1 className="text-center">Oops, something went wrong</h1>
-        <p className="text-center">Please try again later.</p>
+        <h1 className="text-center">Przepraszamy, wystąpił błąd.</h1>
+        <p className="text-center">Proszę spróbować ponownie później.</p>
     </ErrorAlert>
 );
 
